e2e2d402037a14519ae82bdccdbef35001e24282
Philippe Albuquerque <phh.albuquerque@gmail.com>
2026-04-08 16:41:58 +0000

    feat: atualizar componente Home para exibir mensagem personalizada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Tab News Clone!</h1>
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh'}}>
+            <h1 style={{ color: 'red', fontSize: '4rem' }}>Bruna, te amo!❤️</h1>
+        </div>
+    )
 }
 
 export default Home;
