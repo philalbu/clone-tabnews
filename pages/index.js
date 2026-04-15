942481fb970366a5b5c7f201b28ccd253e476b66
Philippe Albuquerque <phh.albuquerque@gmail.com>
2026-04-15 17:03:45 +0000

    mudanças de texto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 function Home() {
     return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh', flexDirection: 'column', gap: '20px'}}>
-            <h1 style={{ color: 'red', fontSize: '4rem', lineHeight: '115%', margin: '0' }}>Bruna, te amo!❤️</h1>
-            <p style={{ color: 'gray', fontSize: '2rem', lineHeight: '150%', margin: '0' }}>Uma mensagem de amor para minha Esposa.</p>
-            <button style={{ backgroundColor: '#144372', padding: '16px 32px', color: '#ffffff'}}>Clique aqui</button>
-        </div>
+        <section style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh', flexDirection: 'column', fontFamily: 'Rubik, sans-serif'}}>
+            <h1 style={{ color: '#2c2c2c', fontSize: '4rem', lineHeight: '115%', margin: '0' }}>Bruna, te amo!❤️</h1>
+            <p style={{ color: '#595959', fontSize: '1.2rem', lineHeight: '150%', margin: '0', marginTop: '8px' }}>Uma palavra de amor para minha Esposa.</p>
+        </section>
     )
 }
 
