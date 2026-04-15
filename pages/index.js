39acfcbf4866b1673bb2fbffe1cac86afe8e9446
Philippe Albuquerque <phh.albuquerque@gmail.com>
2026-04-15 15:49:27 +0000

    Inclusão de botão

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh', flexDirection: 'column', gap: '20px'}}>
             <h1 style={{ color: 'red', fontSize: '4rem', lineHeight: '115%', margin: '0' }}>Bruna, te amo!❤️</h1>
             <p style={{ color: 'gray', fontSize: '2rem', lineHeight: '150%', margin: '0' }}>Uma mensagem de amor para minha Esposa.</p>
+            <button style={{ backgroundColor: '#144372', padding: '16px 32px', color: '#ffffff'}}>Clique aqui</button>
         </div>
     )
 }
