6996195b5ae27f7ee8b52c06310306e7fa578d86
Philippe Albuquerque <phh.albuquerque@gmail.com>
2026-04-15 12:48:09 +0000

    inclusão node_modules na pasta gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh'}}>
-            <h1 style={{ color: 'red', fontSize: '4rem' }}>Bruna, te amo!❤️</h1>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh', flexDirection: 'column', gap: '20px'}}>
+            <h1 style={{ color: 'red', fontSize: '4rem', lineHeight: '115%', margin: '0' }}>Bruna, te amo!❤️</h1>
+            <p style={{ color: 'gray', fontSize: '2rem', lineHeight: '150%', margin: '0' }}>Uma mensagem de amor para minha Esposa.</p>
         </div>
     )
 }
